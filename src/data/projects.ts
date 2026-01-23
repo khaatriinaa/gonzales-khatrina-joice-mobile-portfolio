b86416b61e7de337be71402f1ed60786296e1628
khaatriinaa <khatrinagonzales4@gmail.com>
2026-01-23 21:02:39 +0800

    21st commit

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,7 +4,8 @@ export const projects: Project[] = [
   {
     id: "1",
     title: "FurSure",
-    description: "Pet care platform connecting owners and service providers.",
+    shortDescription: "Pet care platform connecting owners and service providers.",
+    description: "The purpose of this document is to describe the software requirements for FurSure: Your Go-To Pet Care Companion. This document describes the functional and non-functional requirements of the system, including its interfaces, features, performance expectations, and user interaction behavior. FurSure is a mobile application designed to assist pet owners in discovering, booking, and managing essential pet care services such as grooming and veterinary appointments. It also enables service providers to manage their offerings, bookings, and communication with clients.",
   },
   {
     id: "2",
