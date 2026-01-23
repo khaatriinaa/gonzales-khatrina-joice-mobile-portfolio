a95f6b155d5115a66d09ecdc35800aa15f7fbd56
khaatriinaa <khatrinagonzales4@gmail.com>
2026-01-23 19:28:15 +0800

    13th commit

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -12,7 +12,7 @@ export default function ProfileCard({ styles, theme }: Props) {
     <View style={[styles.card, { backgroundColor: theme.card }]}>
       <Image
         source={{
-          uri: "https://scontent-hkg1-1.xx.fbcdn.net/v/t39.30808-1/434149085_3799460017045876_7608266015219454770_n.jpg?...",
+          uri: "https://scontent-hkg1-1.xx.fbcdn.net/v/t39.30808-6/434149085_3799460017045876_7608266015219454770_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=mfBrz1xssRYQ7kNvwFRG-sP&_nc_oc=Adn1Wc3zMZUOuK3IoyU8WiZn2_biwG2mPaO1nzDR6lXi6cPL4fwJ7XgoXAJJ9IP5ecU7e_qb-mw1QW6JHMNsJSar&_nc_zt=23&_nc_ht=scontent-hkg1-1.xx&_nc_gid=wboemzV2g9w8Rb-5jRFrTA&oh=00_Afp5Q5rrilXgQ6a2Fc1rtv_9v8EK0CtJW8x3OeoKdVzeKA&oe=697936F9",
         }}
         style={[
           styles.avatar,
