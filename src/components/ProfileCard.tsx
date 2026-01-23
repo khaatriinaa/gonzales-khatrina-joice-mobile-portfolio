a8cbf846c2fa4c4ceb400dac4d67899f0c2f747e
khaatriinaa <khatrinagonzales4@gmail.com>
2026-01-23 18:46:33 +0800

    11th commit

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -18,7 +18,7 @@ export default function ProfileCard({ styles, theme }: Props) {
       ]}
     >
       <Image
-        source={{ uri: "https://imgur.com/NLvaPYp" }}
+        source={{ uri: "https://scontent-hkg1-1.xx.fbcdn.net/v/t39.30808-1/434149085_3799460017045876_7608266015219454770_n.jpg?stp=cp6_dst-jpg_s160x160_tt6&_nc_cat=109&ccb=1-7&_nc_sid=e99d92&_nc_ohc=mfBrz1xssRYQ7kNvwFRG-sP&_nc_oc=Adn1Wc3zMZUOuK3IoyU8WiZn2_biwG2mPaO1nzDR6lXi6cPL4fwJ7XgoXAJJ9IP5ecU7e_qb-mw1QW6JHMNsJSar&_nc_zt=24&_nc_ht=scontent-hkg1-1.xx&_nc_gid=80g1lwqCEmw_nLsEJ5HoYA&oh=00_AfpJTeNgCQSVQ_9f5x7wxThmMcHHC0MHc8RIKADzMxTagQ&oe=6979173B" }}
         style={[
           styles.avatar,
           {
